2208e6fb0f48380a951605073bad231b6ae8350e
Stephen Kang <ki6080@gmail.com>
2019-09-18 23:51:19 +0900

    Implement event queue using dynamic queue allocator
     @todo. This class not verified yet.

---
+++ src/event-procedure.c
@@ -0,0 +1,61 @@
+#include "event-procedure.h"
+#include <stdlib.h> 
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+//! \todo. Verify functionalities.
+
+#pragma pack(push, 4)
+struct queueArg
+{
+    EventCallbackType func;
+};
+#pragma pack(pop)
+
+inline size_t bundleSize( size_t paramSize )
+{
+    return sizeof( struct queueArg ) + paramSize;
+}
+
+void InitEventProcedure( struct EventQueue* queue, allocator_ref_t allocator, size_t bufferCapacity )
+{
+    queue_allocator_init( &queue->queue, allocator, bufferCapacity );
+}
+
+void FlushEvents( struct EventQueue* queue )
+{
+    while ( queue->queue.cnt )
+    {
+        ProcessEvent( queue );
+    }
+}
+
+void QueueEvent( struct EventQueue* queue, EventCallbackType callback, void const* callbackParam, size_t paramSize )
+{ 
+    struct queueArg* arg = ( struct queueArg* )queue_allocator_push( &queue->queue, bundleSize( paramSize ) );
+    arg->func = callback;
+    void* data = ( void*) ( arg + 1 );
+
+    // Copy parameter data to buffer.
+    memcpy( data, callbackParam, paramSize );
+}
+
+void ProcessEvent( struct EventQueue* queue )
+{
+    // Only a fixed number of events will be processed for each procedure call.
+    size_t fence = queue->queue.head;
+    size_t len;
+
+    while ( queue->queue.tail != fence )
+    {
+        struct queueArg* arg = ( struct queueArg* )queue_allocator_peek( &queue->queue, &len );
+
+        // Call event
+        arg->func( ( void*) ( arg + 1 ) );
+
+        // Proceed parsing tail.
+        queue_allocator_pop( &queue->queue );
+    }
+}
---
+++ src/event-procedure.h
@@ -0,0 +1,46 @@
+/*! \brief Event procedure.
+    \file event-procedure.h
+    \author Seungwoo Kang (ki6080@gmail.com)
+    \version 0.1
+    \date 2019-09-14
+    
+    \copyright Copyright (c) 2019. Seungwoo Kang. All rights reserved.
+    
+    \details 
+ */
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+#include "macro.h"
+#include "queue_allocator.h"
+
+typedef void (*EventCallbackType)(void *);
+
+/*! \brief Queue descriptor. */
+struct EventQueue
+{
+    struct queue_allocator queue;
+};
+
+/*! \brief Initializes event procedure. 
+
+    \param queueSize Allocated memory size of parameter queue.
+    \param buffer Since the dynamic allocation is not recommendable inside of the queue initialization process, the user should provide a specific memory block that will be valid during queue usage. Therefore deallocation of the allocated memory is up to the user.
+    \note Internal queue will be implemented as circular queue. */
+void InitEventProcedure( struct EventQueue* queue, allocator_ref_t allocator, size_t bufferCapacity );
+
+/*! \brief Flush all queue elements. Right after finishing this job, you can release the memory. 
+    \warning If there is an event that repeatedly enqueues itself, this function may not return the program handle. */
+void FlushEvents( struct EventQueue* queue );
+
+/*! \brief Queue new event. 
+    
+    \param callback Callback to execute.
+    \param callbackParam Parameter that will be delivered as callback's parameter. This value will be copied into queue.
+    \param paramSize Size of the parameter. */
+void QueueEvent( struct EventQueue* queue, EventCallbackType callback, void const* callbackParam, size_t paramSize );
+
+/*! \brief Process event.
+    \details
+        This function should be called periodically to process queued events while performing program. To not block whole program control infinitely, a fence object will be set, then it lets the procedure execute only a fixed number of requests. */
+void ProcessEvent( struct EventQueue* queue ); 
