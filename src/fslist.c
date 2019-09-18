43954e338b80e756ab89422790d286d08b345420
Stephen Kang <ki6080@gmail.com>
2019-09-18 23:46:24 +0900

    Rename fslist
     Now struct naming convention follows small snake notation.

--- src/fslist.c
+++ src/fslist.c
@@ -1,35 +1,36 @@
-#include "swk-embedded-c/fslist.h"
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include "fslist.h"
 
 #define assert_idx(idx) assert((idx) < list->poolCapacity)
-static inline void unlink( FreeSpaceList* list, FreeSpaceListNodeHandle a )
-{
-    assert_idx( a );
-    FreeSpaceListNode* n = list->pool + a;
-    
-    if ( n->next != FSLIST_NODEIDX_NONE ) 
-        list->pool[n->next].prev = n->prev; 
 
-    if ( n->prev != FSLIST_NODEIDX_NONE ) 
-        list->pool[n->prev].next = n->next;  
+static inline void unlink(fslist_t *list, fslist_idx_t a)
+{
+    assert_idx(a);
+    fslist_node_t *n = list->pool + a;
+
+    if (n->next != FSLIST_NODEIDX_NONE)
+        list->pool[n->next].prev = n->prev;
+
+    if (n->prev != FSLIST_NODEIDX_NONE)
+        list->pool[n->prev].next = n->next;
 
     n->next = FSLIST_NODEIDX_NONE;
     n->prev = FSLIST_NODEIDX_NONE;
 }
 
-static inline void link_after( FreeSpaceList* list, FreeSpaceListNodeHandle base, FreeSpaceListNodeHandle node )
+static inline void link_after(fslist_t *list, fslist_idx_t base, fslist_idx_t node)
 {
-    assert_idx( base );
-    assert_idx( node );
-    FreeSpaceListNode* b = list->pool + base;
-    FreeSpaceListNode* n = list->pool + node;
+    assert_idx(base);
+    assert_idx(node);
+    fslist_node_t *b = list->pool + base;
+    fslist_node_t *n = list->pool + node;
 
-    unlink( list, node );
-    
-    if ( b->next != FSLIST_NODEIDX_NONE ) 
-        list->pool[b->next].prev = node; 
+    unlink(list, node);
+
+    if (b->next != FSLIST_NODEIDX_NONE)
+        list->pool[b->next].prev = node;
 
     n->next = b->next;
     n->prev = base;
@@ -37,17 +38,17 @@ static inline void link_after( FreeSpaceList* list, FreeSpaceListNodeHandle base
     b->next = node;
 }
 
-static inline void link_before( FreeSpaceList* list, FreeSpaceListNodeHandle base, FreeSpaceListNodeHandle node )
+static inline void link_before(fslist_t *list, fslist_idx_t base, fslist_idx_t node)
 {
-    assert_idx( base );
-    assert_idx( node );
-    FreeSpaceListNode* b = list->pool + base;
-    FreeSpaceListNode* n = list->pool + node;
+    assert_idx(base);
+    assert_idx(node);
+    fslist_node_t *b = list->pool + base;
+    fslist_node_t *n = list->pool + node;
 
-    unlink( list, node );
-    
-    if ( b->prev != FSLIST_NODEIDX_NONE ) 
-        list->pool[b->prev].next = node;  
+    unlink(list, node);
+
+    if (b->prev != FSLIST_NODEIDX_NONE)
+        list->pool[b->prev].next = node;
 
     n->prev = b->prev;
     n->next = base;
@@ -55,47 +56,49 @@ static inline void link_before( FreeSpaceList* list, FreeSpaceListNodeHandle bas
     b->prev = node;
 }
 
-static inline void alloc_data( FreeSpaceList* list, void const* initData, FreeSpaceListNodeHandle target )
+static inline void alloc_data(fslist_t *list, void const *initData, fslist_idx_t target)
 {
-    assert_idx( target );
-    assert( list->pool[target].pdata == NULL );
+    assert_idx(target);
+    assert(list->pool[target].pdata == NULL);
 
-    void* ptr = list->dataPool + list->elemSize * target;
-    list->pool[target].pdata = ptr; 
-    if ( list->ctorCallback )
-        list->ctorCallback( ptr, initData );
-    else if ( initData )
-        memcpy( ptr, initData, list->elemSize );
+    void *ptr = list->dataPool + list->elemSize * target;
+    list->pool[target].pdata = ptr;
+    if (list->ctorCallback)
+        list->ctorCallback(ptr, initData);
+    else if (initData)
+        memcpy(ptr, initData, list->elemSize);
 }
 
-static inline void free_data( FreeSpaceList* list, FreeSpaceListNodeHandle target )
+static inline void free_data(fslist_t *list, fslist_idx_t target)
 {
-    assert_idx( target );
-    assert( list->pool[target].pdata != NULL );
+    assert_idx(target);
+    assert(list->pool[target].pdata != NULL);
 
-    void* ptr = list->dataPool + list->elemSize * target;
+    void *ptr = list->dataPool + list->elemSize * target;
     list->pool[target].pdata = NULL;
-    list->dtorCallback( ptr );
+    list->dtorCallback(ptr);
 }
 
-FreeSpaceListNodeHandle fslistGetOffset( FreeSpaceList const* list, FreeSpaceListNode const* node )
+fslist_idx_t fslist_getOffset(fslist_t const *list, fslist_node_t const *node)
 {
-    if ( node < list->pool || node >= list->pool + list->poolCapacity ) 
-        return FSLIST_NODEIDX_NONE; 
+    if (node < list->pool || node >= list->pool + list->poolCapacity)
+        return FSLIST_NODEIDX_NONE;
     else
-        return ( FreeSpaceListNodeHandle) ( node - list->pool );
+        return (fslist_idx_t)(node - list->pool);
 }
 
-static void nulldtor( void* placeholder ) {}
+static void nulldtor(void *placeholder) {}
 
-bool fslistInit( FreeSpaceList* list, size_t initCapacity, size_t elementSize )
+bool fslist_init(fslist_t *list, allocator_ref_t alloca, size_t initCapacity, size_t elementSize)
 {
-    list->pool = ( FreeSpaceListNode*) malloc( initCapacity * sizeof( FreeSpaceListNode ) );
-    list->dataPool = ( uint8_t*) malloc( initCapacity * elementSize );
+    list->allocatorRef = alloca;
+
+    list->pool = (fslist_node_t *) alloca->allocate(alloca->object, initCapacity * sizeof(fslist_node_t));
+    list->dataPool = (uint8_t *) alloca->allocate( alloca->object, initCapacity * elementSize);
 
     // Memory allcoation has failed ...
-    if ( ( list->pool && list->dataPool ) == false ) 
-        return false; 
+    if ((list->pool && list->dataPool) == false)
+        return false;
 
     list->elemSize = elementSize;
     list->poolCapacity = initCapacity;
@@ -105,14 +108,14 @@ bool fslistInit( FreeSpaceList* list, size_t initCapacity, size_t elementSize )
 
     list->availableHead = 0;
     list->head = list->tail = FSLIST_NODEIDX_NONE;
-    
+
     list->size = 0;
 
-    // Link all available nodes 
+    // Link all available nodes
     // This process is to make searching available node easier.
     {
-        FreeSpaceListNode* pool = list->pool;
-        FreeSpaceListNodeHandle head = 0;
+        fslist_node_t *pool = list->pool;
+        fslist_idx_t head = 0;
 
         // the first and last node need to be initialized separately.
         pool[0].prev = FSLIST_NODEIDX_NONE;
@@ -120,44 +123,44 @@ bool fslistInit( FreeSpaceList* list, size_t initCapacity, size_t elementSize )
         pool[list->poolCapacity - 1].next = FSLIST_NODEIDX_NONE;
 
         // Links nodes iteratively.
-        while ( ++head < initCapacity )
+        while (++head < initCapacity)
         {
-            pool[head - 1].next = head; 
+            pool[head - 1].next = head;
             pool[head].prev = head - 1;
             pool[head].pdata = NULL;
-        } 
+        }
     }
     return true;
 }
 
-bool fslistDestroy( FreeSpaceList* list )
+bool fslist_destroy(fslist_t *list)
 {
     // Destroy all active nodes
     // Destructors will be automatically called.
-    while ( list->head != FSLIST_NODEIDX_NONE )
-        if ( !fslistErase( list, list->head ) )
+    while (list->head != FSLIST_NODEIDX_NONE)
+        if (!fslist_erase(list, list->head))
             return false;
+    
+    list->allocatorRef->release(list->allocatorRef->object, list->pool);
+    list->allocatorRef->release(list->allocatorRef->object, list->dataPool);
 
-    free( list->pool );
-    free( list->dataPool );
-
-    memset( list, 0, sizeof( *list ) );
+    memset(list, 0, sizeof(*list));
     return true;
 }
 
-FreeSpaceListNode* fslistInsertNew( FreeSpaceList* list, FreeSpaceListNodeHandle node, void const* initData )
-{ 
-    FreeSpaceListNodeHandle gen = list->availableHead;
+fslist_node_t *fslist_insertNew(fslist_t *list, fslist_idx_t node, void const *initData)
+{
+    fslist_idx_t gen = list->availableHead;
     list->availableHead = list->pool[gen].next;
-    unlink( list, gen );
+    unlink(list, gen);
     // Mandates handling initdata to ctor.
-    alloc_data( list, initData, gen);
+    alloc_data(list, initData, gen);
 
     // If the case inserting to front.
-    if ( node == FSLIST_NODEIDX_NONE )
+    if (node == FSLIST_NODEIDX_NONE)
     {
-        if ( list->head != FSLIST_NODEIDX_NONE )
-            link_before( list, list->head, gen );
+        if (list->head != FSLIST_NODEIDX_NONE)
+            link_before(list, list->head, gen);
         else // head is empty. Means tail is empty either.
             list->tail = gen;
 
@@ -165,37 +168,37 @@ FreeSpaceListNode* fslistInsertNew( FreeSpaceList* list, FreeSpaceListNodeHandle
     }
     else
     {
-        assert_idx( node );
-        assert( list->pool[node].pdata == NULL );
-        if ( list->pool[node].pdata == NULL )
+        assert_idx(node);
+        assert(list->pool[node].pdata == NULL);
+        if (list->pool[node].pdata == NULL)
             return NULL;
 
-        link_after( list, node, gen );
+        link_after(list, node, gen);
 
-        if ( list->tail == node )
+        if (list->tail == node)
             list->tail = gen;
     }
-    
+
     ++list->size;
     return list->pool + gen;
 }
 
-bool fslistErase( FreeSpaceList* list, FreeSpaceListNodeHandle node )
+bool fslist_erase(fslist_t *list, fslist_idx_t node)
 {
-    assert_idx( node );
-    assert( list->pool[node].pdata != NULL );
+    assert_idx(node);
+    assert(list->pool[node].pdata != NULL);
 
-    if ( node == list->head )
+    if (node == list->head)
         list->head = list->pool[node].next;
 
-    if ( node == list->tail )
+    if (node == list->tail)
         list->tail = list->pool[node].prev;
 
-    FreeSpaceListNodeHandle avhead = list->availableHead;
+    fslist_idx_t avhead = list->availableHead;
     list->availableHead = node;
-    link_before( list, avhead, node );
+    link_before(list, avhead, node);
 
-    free_data( list, node );
+    free_data(list, node);
 
     --list->size;
     return true;
--- testcase/test-fslist.cpp
+++ testcase/test-fslist.cpp
@@ -1,82 +1,84 @@
 extern "C"
 {
-#include "swk-embedded-c/fslist.h"
+#include "fslist.h"
 }
 #include "catch2/catch.hpp"
+#include "allocator.h"
 #include <vector>
 #include <cstdlib>
 #include <tuple>
 
-TEST_CASE( "fslist features perform correctly.", "[fslist]" )
+TEST_CASE("fslist features perform correctly.", "[fslist]")
 {
-    FreeSpaceList list;
-    
-    enum {
+    fslist_t list;
+
+    enum
+    {
         CAP = 10000
     };
-    srand( CAP );
-    fslistInit( &list, CAP, sizeof( double ) );
+    srand(CAP);
+    fslist_init(&list, &g_mallocator, CAP, sizeof(double));
 
-    INFO( "Testing parameter setups." );
-    REQUIRE( list.size == 0 );
-    REQUIRE( list.availableHead == 0 );
-    REQUIRE( list.dataPool != NULL );
-    REQUIRE( list.pool != NULL );
-    REQUIRE( list.elemSize == sizeof( double ) );
-    REQUIRE( list.poolCapacity == CAP );
-     
-    INFO( "Testing linkage." );
-    for ( size_t i = 0; i < CAP-1; i++ )
+    INFO("Testing parameter setups.");
+    REQUIRE(list.size == 0);
+    REQUIRE(list.availableHead == 0);
+    REQUIRE(list.dataPool != NULL);
+    REQUIRE(list.pool != NULL);
+    REQUIRE(list.elemSize == sizeof(double));
+    REQUIRE(list.poolCapacity == CAP);
+
+    INFO("Testing linkage.");
+    for (size_t i = 0; i < CAP - 1; i++)
     {
-        REQUIRE( list.pool[i].next == i+1 );
-        REQUIRE( list.pool[i + 1].prev == i );
-        REQUIRE( list.pool[i].pdata == NULL );
+        REQUIRE(list.pool[i].next == i + 1);
+        REQUIRE(list.pool[i + 1].prev == i);
+        REQUIRE(list.pool[i].pdata == NULL);
     }
-    REQUIRE( list.pool[0].prev == FSLIST_NODEIDX_NONE );
-    REQUIRE( list.pool[CAP - 1].next == FSLIST_NODEIDX_NONE );
-    REQUIRE( list.pool[CAP - 1].pdata == NULL ); 
+    REQUIRE(list.pool[0].prev == FSLIST_NODEIDX_NONE);
+    REQUIRE(list.pool[CAP - 1].next == FSLIST_NODEIDX_NONE);
+    REQUIRE(list.pool[CAP - 1].pdata == NULL);
 
     // Allocate few nodes
-    std::vector<std::pair<double, FreeSpaceListNodeHandle>> pairs;
-    for ( size_t i = 0, length = rand() % CAP; i < length; i++ )
+    std::vector<std::pair<double, fslist_idx_t>> pairs;
+    for (size_t i = 0, length = rand() % CAP; i < length; i++)
     {
         double v = rand();
 
-        auto idx = fslistGetOffset( &list, fslistInsertNew( &list, FSLIST_NODEIDX_NONE, &v ));
-        REQUIRE( idx != FSLIST_NODEIDX_NONE );
-        REQUIRE( idx == i );
+        auto idx = fslist_getOffset(&list, fslist_insertNew(&list, FSLIST_NODEIDX_NONE, &v));
+        REQUIRE(idx != FSLIST_NODEIDX_NONE);
+        REQUIRE(idx == i);
 
-        pairs.emplace_back( std::make_pair( v, idx ) );
+        pairs.emplace_back(std::make_pair(v, idx));
     }
-    
-    INFO( "Testing allocated counts." );
-    REQUIRE( pairs.size() == list.size );
 
-    INFO( "Allocated data is correct." );
-    for ( auto& p : pairs )
+    INFO("Testing allocated counts.");
+    REQUIRE(pairs.size() == list.size);
+
+    INFO("Allocated data is correct.");
+    for (auto &p : pairs)
     {
-        REQUIRE( *( double*) list.pool[p.second].pdata == p.first );
+        REQUIRE(*(double *)list.pool[p.second].pdata == p.first);
     }
 
-    SECTION( "Deallocated all nodes randomly" )
+    SECTION("Deallocated all nodes randomly")
     {
         int numToRelease = rand() % pairs.size();
-        while ( numToRelease )
+        while (numToRelease)
         {
             int idx = rand() % numToRelease--;
-            REQUIRE( fslistErase( &list, pairs[idx].second ) );
-            pairs.erase( pairs.begin() + idx );
-            REQUIRE( list.size == pairs.size() );
+            REQUIRE(fslist_erase(&list, pairs[idx].second));
+            pairs.erase(pairs.begin() + idx);
+            REQUIRE(list.size == pairs.size());
         }
     }
 
-    SECTION( "Destructors are called correctly." )
+    SECTION("Destructors are called correctly.")
     {
         static int dtorcnt = 0;
-        auto dtor = [] ( void* ) { dtorcnt++; };
+        auto dtor = [](void *) { dtorcnt++; };
         list.dtorCallback = dtor;
-        
-        REQUIRE( fslistDestroy( &list ) );
-        REQUIRE( dtorcnt == pairs.size() );
+
+        REQUIRE(fslist_destroy(&list));
+        REQUIRE(dtorcnt == pairs.size());
     }
 }
