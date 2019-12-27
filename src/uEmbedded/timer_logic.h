b7191ec81558c07120216ace88fb205d5a4e5669
Stephen Kang <ki6080@gmail.com>
2019-12-28 01:32:24 +0900

    Modify comment type
     = /// is recognized as XML comments in VS,
     thus changed into //! to prevent unnecessary
     collision.

--- src/uEmbedded/timer_logic.h
+++ src/uEmbedded/timer_logic.h
@@ -39,7 +39,7 @@ timer_handle_t timer_add(timer_logic_t *s, size_t        whenToTrigger,
                          void (*callback)(void *), void *callbackObj);
 
 //! \brief      Update timer based on given time parameter.
-/// @returns    Next trigger time. -1 if there's no more timer to trigger.
+//! @returns    Next trigger time. -1 if there's no more timer to trigger.
 size_t timer_update(timer_logic_t *s, size_t curTime);
 
 //! \brief      Get closest timer's trigger time
