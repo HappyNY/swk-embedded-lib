9d54056a95617492c48d8a64ab0e0bd07461681f
Stephen Kang <ki6080@gmail.com>
2020-01-26 20:08:11 +0900

    Implement timer logic for C++

---
+++ src/uEmbedded-pp/static_timer_logic.hxx
@@ -0,0 +1,124 @@
+#pragma once
+#include "static_fslist.hxx"
+#include <algorithm>
+#include <functional>
+#include <optional>
+#include <stdint.h>
+
+namespace upp {
+using timer_cb_t = void ( * )( void* );
+enum { TIMER_INVALID = -1 };
+
+namespace impl {
+
+template <typename tick_ty__>
+struct timer_logic_desc {
+    tick_ty__  id_;
+    tick_ty__  trigger_at_;
+    void*      obj_;
+    timer_cb_t cb_;
+};
+
+template <typename tick_ty__, typename it__>
+struct timer_handle {
+    tick_ty__ id_;
+    it__      at_;
+};
+
+} // namespace impl
+
+template <typename tick_ty__,
+          typename size_ty__,
+          size_t max_node__>
+class timer_logic {
+public:
+    using desc_type      = impl::timer_logic_desc<tick_ty__>;
+    using tick_type      = tick_ty__;
+    using size_type      = size_ty__;
+    using tick_fnc_type  = std::function<tick_type( void )>;
+    using container_type = upp::static_fslist<desc_type, size_type, max_node__>;
+    using handle_type    = impl::timer_handle<tick_ty__, typename container_type::const_iterator>;
+
+public:
+    tick_fnc_type const& tick_function() const { return tick_; }
+    template <class tick_fnc__>
+    void tick_function( tick_fnc__&& v ) { tick_ = std::forward<tick_fnc__>( v ); }
+
+    handle_type add( tick_type delay, void* obj, timer_cb_t callback ) noexcept
+    {
+        uassert( tick_ );
+        uassert( capacity() );
+
+        desc_type d;
+        d.trigger_at_ = delay + tick_();
+        d.cb_         = callback;
+        d.obj_        = obj;
+        d.id_         = id_gen_++;
+
+        auto at = std::find_if(
+            node_.begin(), node_.end(),
+            [&d]( auto& a ) { return d.trigger_at_ < a.trigger_at_; } );
+
+        auto it = node_.insert( at, d );
+
+        handle_type ret;
+        ret.at_ = it;
+        ret.id_ = d.id_;
+
+        return ret;
+    }
+
+    bool remove( handle_type t ) noexcept
+    {
+        if ( t.at_.valid() &&
+             t.at_->id_ == t.id_ ) {
+            node_.erase( t.at_ );
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    tick_type next_trig() const noexcept
+    {
+        if ( empty() ) {
+            return TIMER_INVALID;
+        }
+        return node_.front().trigger_at_;
+    }
+
+    std::optional<desc_type> browse( handle_type t ) const noexcept
+    {
+        if ( t.at_ &&
+             t.at_->id_ == t.id_ ) {
+            return *t.at_;
+        }
+        return {};
+    }
+
+    tick_type update() noexcept
+    {
+        for ( auto it = node_.begin();
+              it && it->trigger_at_ <= tick_();
+              it = node_.begin() ) {
+            auto cb  = it->cb_;
+            auto obj = it->obj_;
+
+            node_.pop_front();
+            cb( obj );
+        }
+
+        return next_trig();
+    }
+
+    size_type capacity() const noexcept { return node_.capacity(); }
+    size_type size() const noexcept { return node_.size(); }
+    bool      empty() const noexcept { return node_.empty(); }
+
+private:
+    container_type node_;
+    tick_fnc_type  tick_;
+    tick_type      id_gen_ = 0;
+};
+} // namespace upp
--- testcase/test-timer_logic.cpp
+++ testcase/test-timer_logic.cpp
@@ -1,35 +1,38 @@
 #include <Catch2/catch.hpp>
-extern "C"
-{
+extern "C" {
 #include <uEmbedded/timer_logic.h>
 }
+
+#include <uEmbedded-pp/static_timer_logic.hxx>
 
 TEST_CASE( "Timer logic functionality test", "[timer-logic]" )
 {
     timer_logic s;
     enum { CAP = 0x1000 };
-    auto cnt = timer_init( &s, malloc( CAP ), CAP );
+    auto           cnt = timer_init( &s, malloc( CAP ), CAP );
     timer_handle_t h[20];
 
     double v = 0.0f;
 
-    h[0] = timer_add( &s, 2000, [] ( void* o ) {*( double*) o = 2.0; }, & v );
-    h[1] = timer_add( &s, 1000, [] ( void* o ) {*( double*) o = 1.0; }, & v );
-    auto x = h[2] = timer_add( &s, 3000, [] ( void* o ) {*( double*) o = 3.0; }, &v );
+    h[0] = timer_add(
+        &s, 2000, []( void* o ) { *(double*)o = 2.0; }, &v );
+    h[1] = timer_add(
+        &s, 1000, []( void* o ) { *(double*)o = 1.0; }, &v );
+    auto x = h[2] = timer_add(
+        &s, 3000, []( void* o ) { *(double*)o = 3.0; }, &v );
 
     REQUIRE( timer_nextTrigger( &s ) == 1000 );
     REQUIRE( timer_browse( &s, x )->triggerTime == 3000 );
     REQUIRE( timer_browse( &s, x )->timerId == 2 );
 
-    for ( size_t i = 0, length = 10; i < length; i++ )
-    {
-        h[3 + i] = timer_add( &s, 4000 + i * 1000, [] ( void* o ) { *( double*) o += 1.0; }, &v );
+    for ( size_t i = 0, length = 10; i < length; i++ ) {
+        h[3 + i] = timer_add(
+            &s, 4000 + i * 1000, []( void* o ) { *(double*)o += 1.0; }, &v );
     }
 
     SECTION( "Update timers" )
     {
-        for ( size_t i = 0; i < 14; ++i )
-        {
+        for ( size_t i = 0; i < 14; ++i ) {
             timer_update( &s, i * 1000 );
             REQUIRE( v == i );
         }
@@ -43,4 +46,45 @@ TEST_CASE( "Timer logic functionality test", "[timer-logic]" )
         timer_update( &s, 100000 );
         REQUIRE( v == 10.0f );
     }
+}
+
+TEST_CASE( "Timer logic cpp version test", "[timer-logic]" )
+{
+    upp::timer_logic<uint64_t, uint8_t, 128> tim;
+    decltype( tim )::tick_type               ticks = 0;
+    std::vector<int>                         tester;
+
+    tim.tick_function( [&ticks]() { return ticks; } );
+
+    tim.add( 1000, &tester, []( void* o ) { ( (std::vector<int>*)o )->push_back( 3 ); } );
+    tim.add( 2000, &tester, []( void* o ) { ( (std::vector<int>*)o )->push_back( 3 ); } );
+    auto rmv = tim.add( 3000, &tester, []( void* o ) { ( (std::vector<int>*)o )->push_back( 3 ); } );
+    tim.add( 5000, &tester, []( void* o ) { ( (std::vector<int>*)o )->push_back( 3 ); } );
+    tim.add( 6000, &tester, []( void* o ) { ( (std::vector<int>*)o )->push_back( 3 ); } );
+
+    REQUIRE( tim.size() == 5 );
+    REQUIRE( tester.size() == 0 );
+    tim.update();
+    REQUIRE( tester.size() == 0 );
+
+    ticks = 1000;
+    tim.update();
+    REQUIRE( tester.size() == 1 );
+
+    tim.update();
+    REQUIRE( tim.size() == 4 );
+    REQUIRE( tester.size() == 1 );
+
+    REQUIRE( tim.remove( rmv ) );
+    REQUIRE( tim.size() == 3 );
+
+    ticks = 3001;
+    tim.update();
+    REQUIRE( tester.size() == 2 );
+    REQUIRE( tim.size() == 2 );
+    
+    ticks = 6000;
+    tim.update();
+    REQUIRE( tester.size() == 4 );
+    REQUIRE( tim.size() == 0 );
 }
