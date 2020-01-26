624c262db64d2e7a5ff8aa8694c4add1092e0f95
Stephen Kang <ki6080@gmail.com>
2020-01-26 14:54:43 +0900

    Change assert function default impl

--- src/uEmbedded/uassert.c
+++ src/uEmbedded/uassert.c
@@ -1,14 +1,2 @@
 #include <assert.h>
 #include <stdio.h>
-
-void uemb_assert_impl_default(char const *expr, char const *file,
-                              char const *function, int line)
-{
-    fprintf(stderr, "<<< ASSERT >>> \n\t%s() on line %d: %s \n\t EXPR ... %s",
-            function, line, file, expr);
-    assert(0 && expr);
-}
-
-void (*uemb_assert_impl)(char const *expr, char const *file,
-                         char const *function,
-                         int         line) = uemb_assert_impl_default;
