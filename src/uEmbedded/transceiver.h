da673bbd54c67e901eba7919b50edea41ad4ff49
Stephen Kang <ki6080@gmail.com>
2020-01-27 12:37:57 +0900

    Correct transceiver operation

--- src/uEmbedded/transceiver.h
+++ src/uEmbedded/transceiver.h
@@ -48,6 +48,11 @@ struct transceiver_vtable {
 //! Alias to easy use.
 typedef struct transceiver_vtable transceiver_vtable_t;
 
+//! Actual transceiver desriptor type
+typedef struct tranceiver_desc {
+    transceiver_vtable_t* vt_;
+}* tr_desc_t__;
+
 /*! \brief      The base type of all transceiver implementations.
     \details
       This base type definition is recommended to be placed as the first member
@@ -62,28 +67,28 @@ typedef intptr_t transceiver_descriptor_t;
 //! Read data from the transceiver.
 static inline transceiver_result_t td_read( transceiver_descriptor_t desc, char* buf, size_t rdcnt )
 {
-    transceiver_vtable_t* td = (transceiver_vtable_t*)desc;
-    return td->read( (void*)td, buf, rdcnt );
+    transceiver_vtable_t* td = ((tr_desc_t__)desc)->vt_;
+    return td->read( (void*)desc, buf, rdcnt );
 }
 
 //! Write data into the transceiver
 static inline transceiver_result_t td_write( transceiver_descriptor_t desc, char* buf, size_t wrcnt )
 {
-    transceiver_vtable_t* td = (transceiver_vtable_t*)desc;
+    transceiver_vtable_t* td = ( (tr_desc_t__) desc )->vt_;
     return td->write( (void*)td, buf, wrcnt );
 }
 
 //! Deliever control command to transceiver
 static inline transceiver_result_t td_ioctl( transceiver_descriptor_t desc, intptr_t cmd )
 {
-    transceiver_vtable_t* td = (transceiver_vtable_t*)desc;
+    transceiver_vtable_t* td = ( (tr_desc_t__) desc )->vt_;
     return td->ioctl( td, cmd );
 }
 
 //! Close transceiver
 static inline transceiver_result_t td_close( transceiver_descriptor_t desc )
 {
-    transceiver_vtable_t* td = (transceiver_vtable_t*)desc;
+    transceiver_vtable_t* td = ( (tr_desc_t__) desc )->vt_;
     return td->close( td );
 }
 #ifdef __cplusplus
