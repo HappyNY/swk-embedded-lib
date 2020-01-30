7c6418a338d2af62d41612a3ce20607cdb64999d
Stephen Kang <ki6080@gmail.com>
2020-01-30 14:45:02 +0900

    Add new runtime hash function

--- src/uEmbedded-pp/utility.hxx
+++ src/uEmbedded-pp/utility.hxx
@@ -3,7 +3,7 @@
 #if __cplusplus < 201700L
 #include <experimental/optional>
 #else
-#include <optional> 
+#include <optional>
 #endif
 
 #include <stdint.h>
@@ -21,9 +21,9 @@ constexpr uint32_t prime_32_const = 0x1000193;
 constexpr uint64_t val_64_const   = 0xcbf29ce484222325;
 constexpr uint64_t prime_64_const = 0x100000001b3;
 
-inline constexpr uint32_t fnv1a_32( const char* const str, const uint32_t value = val_32_const ) noexcept
+inline constexpr uint32_t fnv1a_32_const( const char* const str, const uint32_t value = val_32_const ) noexcept
 {
-    return ( str[0] == '\0' ) ? value : fnv1a_32( &str[1], ( value ^ uint32_t( str[0] ) ) * prime_32_const );
+    return ( str[0] == '\0' ) ? value : fnv1a_32_const( &str[1], ( value ^ uint32_t( str[0] ) ) * prime_32_const );
 }
 
 inline constexpr uint64_t fnv1a_64( const char* const str, const uint64_t value = val_64_const ) noexcept
@@ -31,9 +31,24 @@ inline constexpr uint64_t fnv1a_64( const char* const str, const uint64_t value 
     return ( str[0] == '\0' ) ? value : fnv1a_64( &str[1], ( value ^ uint64_t( str[0] ) ) * prime_64_const );
 }
 
+inline const uint32_t fnv1a_32( const void* key )
+{
+    const char* data  = (char*)key;
+    uint32_t    hash  = 0x811c9dc5;
+    uint32_t    prime = 0x1000193;
+
+    for ( ; *data; ++data ) {
+        hash = hash ^ *data;
+        hash *= prime;
+    }
+
+    return hash;
+
+} //hash_32_fnv1a
+
 } // namespace hash
 
-#if __cplusplus < 201700L 
+#if __cplusplus < 201700L
 template <class ty__>
 using optional = std::experimental::optional<ty__>;
 #else
